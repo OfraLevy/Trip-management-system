48d9eede9086e32a9409842f4f80f551289233d0
OfraLevy <ofra1levy@gmail.com>
2026-04-24 11:00:46 +0300

    creating backend

---
+++ backend/src/test/java/com/trip_mn_sys/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trip_mn_sys;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
